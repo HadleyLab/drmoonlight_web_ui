23c968d2a697e45c682f6d1ccdc35bd82197d082
Elena Grednikova <vesnushkalena@gmail.com>
2017-11-23 16:40:31 +0700

    Dashbpard styles

--- srcs/ui/dashboard/core.cljs
+++ srcs/ui/dashboard/core.cljs
@@ -5,25 +5,22 @@
    [ui.pages :as pages]
    [re-frame.core :as rf]
    [clojure.string :as str]
+   [soda-ash.core :as sa]
    [sodium.core :as na]))
 
 (defn index [params]
-  [na/grid {}
-   [na/grid-row {}
-    [na/grid-column {:width 4}
-     [na/header {} "Dr. Moonlight"]]
-    [na/grid-column {:width 8} [:div]]
-    [na/grid-column {:width 4}
-     [na/button {:basic? true :color :blue :content "Log In" :on-click (na/>event [:goto :login])}]
-     [na/button {:color :blue :content "Sign up" :on-click (na/>event [:goto :sign-up])}]]
-    ]
-   [na/grid-row {}
-    [na/grid-column {:width 4} [:div]]
-    [na/grid-column {:width 8}
-     [na/container {:text? true}
-      [na/header {} "Medical moonlighting ma easy"]
-      [:p "Find greateclinical jobs without agency"]]]
-    [na/grid-column {:width 4} [:div]]]
-   [na/grid-row {}]])
+  [sa/Grid {:container true :padded "vertically" :class-name "full-screen flex-direction _column"}
+   [sa/GridRow {}
+    [sa/GridColumn {:width 4}
+     [sa/Header {} "Dr. Moonlight"]]
+    [sa/GridColumn {:width 12 :text-align "right"}
+     [sa/Button {:basic true :color :blue :content "Log In" :on-click (na/>event [:goto :login])}]
+     [sa/Button {:color :blue :content "Sign up" :on-click (na/>event [:goto :sign-up])}]]]
+   [sa/GridRow {:centered true :class-name "flex _1 align-items _center"}
+    [:div {:class-name "dashboard-header"}
+     [sa/Header {:as "h1" :text-align "left"}
+      [sa/HeaderContent {}
+       [:div "Medical moonlighting made easy"]
+       [sa/HeaderSubheader "Find great clinical jobs without agency recruiters"]]]]]])
 
 (pages/reg-page :core/index index)
--- srcs/ui/styles.cljs
+++ srcs/ui/styles.cljs
@@ -9,15 +9,27 @@
 
 (def basic-style
   (garden/css
+   ; common styles
    [:body {:background-color "#F3F8FC"}]
-   [:.error {:color :red}]
+   [:.full-screen {:height "100vh"
+                   :min-height (px 600)}]
+   [:.flex-direction._column {:flex-direction "column"}]
+   [:.flex._1 {:flex 1}]
+   [:.align-items._center
+    :.row.align-items._center {:align-items "center"}]
    [:.moonlight-white {:background-color "#FFF"}]
    [:.moonlight-header {:height (px 80)
                         :background-color "#FFF"
                         :margin-bottom (px 35)
                         :box-shadow "0 2px 1px -1px gray"}]
+
+   ; dashboard styles
+   [:.dashboard-header {:margin-top (px -100)}]
+
+   ; form styles
+   [:.error {:color :red}]
    [:div.moonlight-form-wrapper {:padding-top (px 70)}]
-                                  ;; :padding-bottom (px 70)}]
+                                  ; :padding-bottom (px 70)}]
    [:h1.moonlight-form-header {:margin-top "20px!important"
                                :margin-left "100px!important"}]
    [:div.moonlight-form {:background-color "#FFF"
