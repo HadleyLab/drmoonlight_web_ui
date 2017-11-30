aa7aca1d05a84e0be4a6652a1f292c199fdeb400
Elena Grednikova <vesnushkalena@gmail.com>
2017-11-30 20:15:37 +0700

    Update params destructuring

--- srcs/ui/widgets/error-message.cljs
+++ srcs/ui/widgets/error-message.cljs
@@ -3,7 +3,7 @@
    [soda-ash.core :as sa]
    [clojure.string :as str]))
 
-(defn getMessage [error names-map names-list]
+(defn get-message [error names-map names-list]
   (let [field (find names-map (key error))
         field-name (or (if field (val field)) (str/capitalize (name (key error))))
         error-text (first (val error))]
@@ -11,12 +11,9 @@
       (str field-name ":" " " error-text)
       error-text)))
 
-(defn ErrorMessage [params]
-  (let [visible (:visible params)
-        errors (:errors params)
-        field-names-map (:field-names-map params)
-        field-names-list (keys errors)]
+(defn ErrorMessage [{:keys [visible errors field-names-map]}]
+  (let [field-names-list (keys errors)]
     [sa/Message {:error true
                  :visible visible
                  :header "There was some errors with your submission"
-                 :list (map (fn [error] (getMessage error field-names-map field-names-list)) errors)}]))
+                 :list (map (fn [error] (get-message error field-names-map field-names-list)) errors)}]))
