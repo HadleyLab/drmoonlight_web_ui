ca78770983f084a0240e1ca8d9475c9b1e08cd2b
Elena Grednikova <vesnushkalena@gmail.com>
2017-12-01 18:30:06 +0700

    Update header styles

--- srcs/ui/resident/layout.cljs
+++ srcs/ui/resident/layout.cljs
@@ -1,40 +1,19 @@
 (ns ui.resident.layout
   (:require
-   [ui.db.account :refer [is-approved is-new]]
+   [ui.db.account :refer [is-new]]
    [ui.db.misc :refer [>evt <sub]]
-   [re-frame.core :as rf]
-   [soda-ash.core :as sa]))
+   [soda-ash.core :as sa]
+   [ui.widgets.header :refer [ResidentHeader]]))
 
 (defn ResidentLayout [content]
   (let [route (<sub [:route-map/current-route])]
     [sa/Grid {}
-     [sa/GridRow {:class-name "header__wrapper"}
-      [sa/Container {:class-name "header__content"}
-       [sa/Header {} "Dr.Moonlight"]
-       [sa/ButtonGroup {}
-        [sa/Button {:active (= (:match route) :core/resident-schedule)
-                    :on-click #(>evt [:goto :resident :schedule])}
-         [sa/Icon {:name :calendar}]
-         "Schedule"]
-        [sa/Button {:active (= (:. (nth (:parents route) 2 nil))
-                               :core/resident-messages)
-                    :disabled (not (is-approved))
-                    :on-click #(>evt [:goto :resident :messages])}
-         [sa/Icon {:name :mail}]
-         "Messages"]
-        [sa/Button {:active (= (:match route) :core/resident-statistics)
-                    :on-click #(>evt [:goto :resident :statistics])}
-         [sa/Icon {:name :table}]
-         "Statistics"]
-        [sa/Button {:active (= (:. (last (:parents route))) :core/resident-profile)
-                    :on-click #(>evt [:goto :resident :profile])}
-         [sa/Icon {:name :user}]
-         "Profile"]]]]
+     [ResidentHeader]
      [sa/GridRow {}
       [sa/Container content]]]))
 
 (defn ResidentProfileLayout [content]
-    (let [route (<sub [:route-map/current-route])]
+  (let [route (<sub [:route-map/current-route])]
     [ResidentLayout
      [sa/Grid {}
       [sa/GridRow {}
@@ -45,10 +24,11 @@
                         :active (= (:match route) :core/resident-profile)
                         :on-click #(>evt [:goto :resident :profile])}]
           (when-not (is-new) [sa/MenuItem {:name "Notification Settings"
-                        :active (= (:match route) :core/resident-profile-notification)
-                        :on-click #(>evt [:goto :resident :profile :notification])}])
+                                           :active (= (:match route) :core/resident-profile-notification)
+                                           :on-click #(>evt [:goto :resident :profile :notification])}])
           [sa/MenuItem {:name "Log out"
                         :on-click #(>evt [:logout])}]]]]
 
        [sa/GridColumn {:width 12}
-        [:div {:class-name "profile__container moonlight-white"} content]]]]]))
+        [:div {:class-name "profile__container moonlight-white"} content]]]
+      [sa/GridRow {}]]]))
--- srcs/ui/scheduler/layout.cljs
+++ srcs/ui/scheduler/layout.cljs
@@ -1,37 +1,12 @@
 (ns ui.scheduler.layout
   (:require
    [ui.db.misc :refer [<sub]]
-   [re-frame.core :as rf]
-   [soda-ash.core :as sa]))
+   [soda-ash.core :as sa]
+   [ui.widgets.header :refer [SchedulerHeader]]))
 
 (defn SchedulerLayout [content]
   (let [route (<sub [:route-map/current-route])]
     [sa/Grid {}
-     [sa/GridRow {:class-name "moonlight-header"}
-      [sa/GridColumn {:width 1}]
-      [sa/GridColumn {:width 7} [sa/Header {} "Dr.Moonlight"]]
-      [sa/GridColumn {:width 7}
-       [sa/ButtonGroup {}
-        [sa/Button {:active (= (:match route) :core/scheduler-schedule)
-                    :on-click #(rf/dispatch [:goto :scheduler :schedule])}
-         [sa/Icon {:name :calendar}]
-         "Schedule"]
-
-        [sa/Button {:active (= (:. (nth (:parents route) 2 nil))
-                               :core/scheduler-messages)
-                    :on-click #(rf/dispatch [:goto :scheduler :messages])}
-         [sa/Icon {:name :mail}]
-         "Messages"]
-        [sa/Button {:active (= (:match route) :core/scheduler-statistics)
-                    :on-click #(rf/dispatch [:goto :scheduler :statistics])}
-         [sa/Icon {:name :table}]
-         "Statistics"]
-        [sa/Button {:active (= (:. (last (:parents route))) :core/scheduler-profile)
-                    :on-click #(rf/dispatch [:goto :scheduler :profile])}
-         [sa/Icon {:name :user}]
-         "Profile"]]]]
+     [SchedulerHeader]
      [sa/GridRow {}
-      [sa/GridColumn {:width 1}]
-      [sa/GridColumn {:width 14} content]
-      [sa/GridColumn {:width 1}]]]))
-
+      [sa/Container content]]]))
