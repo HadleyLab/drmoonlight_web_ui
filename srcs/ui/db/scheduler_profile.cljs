19023b57221444a535dc168e2759fbc589a8e720
Elena Grednikova <vesnushkalena@gmail.com>
2017-12-08 17:08:00 +0700

    Add scheduler profile page

--- srcs/ui/db/scheduler_profile.cljs
+++ srcs/ui/db/scheduler_profile.cljs
@@ -1,12 +1,56 @@
 (ns ui.db.scheduler-profile
   (:require
-   [re-frame.core :as rf]))
+   [re-frame.core :as rf]
+   [ui.db.misc :refer [get-url <sub fields->schema
+                       setup-form-initial-values]]))
+
+(def scheduler-profile-form-fields
+  {"Personal Information" {:first-name "First Name"
+                           :last-name "Last Name"}
+   "Residency" (array-map
+                :facility-name {:type :input
+                                :label "Hospital / Facility name"}
+                :department-name {:type :input
+                                  :label "Department name"})})
 
 (def schema
   {::scheduler-profile
-   {:profile-form {}}})
+   {:profile-form
+    {:fields
+     (fields->schema scheduler-profile-form-fields)
+     :response {:status :not-asked}}}})
 
 (rf/reg-sub
  ::scheduler-profile
  (fn [db path]
    (get-in db path)))
+
+(rf/reg-event-db
+ :init-scheduler-profile-form
+ (fn [db _]
+   (-> db
+       (assoc-in [::scheduler-profile :profile-form]
+                 (get-in schema [::scheduler-profile :profile-form]))
+       (update-in [::scheduler-profile :profile-form :fields]
+                  (setup-form-initial-values (<sub [:user-info]))))))
+
+(rf/reg-sub
+ :scheduler-profile-form-cursor
+ #(<sub [:cursor [::scheduler-profile :profile-form]]))
+
+(rf/reg-sub
+ :scheduler-profile-form-response
+ #(<sub [::scheduler-profile :profile-form :response]))
+
+(rf/reg-event-fx
+ :update-scheduler-profile
+ (fn [{db :db} [_ type]]
+   (let [pk (<sub [:user-id])
+         state (<sub [:user-state])
+         body (<sub [::scheduler-profile :profile-form :fields])
+         url (get-url db "/api/accounts/scheduler/" pk "/")]
+     {:json/fetch->path {:path [::scheduler-profile :profile-form :response]
+                         :uri url
+                         :method (if (= state 1) "POST" "PATCH")
+                         :token (<sub [:token])
+                         :body body}})))
--- srcs/ui/scheduler/core.cljs
+++ srcs/ui/scheduler/core.cljs
@@ -3,18 +3,38 @@
    [reagent.core :as reagent]
    [ui.pages :as pages]
    [ui.routes :refer [href]]
+   [ui.db.misc :refer [<sub >event]]
    [ui.scheduler.layout :refer [SchedulerLayout SchedulerProfileLayout]]
+   [ui.db.scheduler-profile :refer [scheduler-profile-form-fields]]
    [re-frame.core :as rf]
    [clojure.string :as str]
    [soda-ash.core :as sa]
+   [ui.widgets :refer [BuildForm]]
    [ui.scheduler.schedule.core]
    [ui.widgets.resident-profile-detail :refer [ResidentProfileDetail]]
    [ui.scheduler.message]))
+
+(defn SchedulerProfileForm []
+  (rf/dispatch [:init-scheduler-profile-form])
+  (let [scheduler-profile-form-cursor (<sub [:scheduler-profile-form-cursor])]
+    (fn []
+      (let [{status :status errors :errors} (<sub [:scheduler-profile-form-response])
+            email (<sub [:user-email])]
+        [:div.profile__content._edit
+         [sa/Form {:class-name "moonlight-form-inner"}
+          [BuildForm scheduler-profile-form-cursor scheduler-profile-form-fields]
+          [:div.form__group._account-settings
+           [:label.form__group-title "Account settings"]
+           [:div.field [:label "Email"] [:p email]]
+           [sa/FormButton {:color :blue
+                           :loading (= status :loading)
+                           :class-name "profile__button"
+                           :on-click (>event [:update-scheduler-profile])} "Save changes"]]]]))))
 
 (defn WrappedResidentProfileDetail [params]
   [SchedulerLayout [ResidentProfileDetail params]])
 
 (pages/reg-page :core/scheduler (fn [] [SchedulerLayout [sa/Header {} "index"]]))
 (pages/reg-page :core/scheduler-statistics (fn [] [SchedulerLayout [sa/Header {} "statistics"]]))
-(pages/reg-page :core/scheduler-profile (fn [] [SchedulerProfileLayout [:div "Scheduler profile"]]))
+(pages/reg-page :core/scheduler-profile (fn [] [SchedulerProfileLayout [SchedulerProfileForm]]))
 (pages/reg-page :scheduler/resident-profile-detail WrappedResidentProfileDetail)
